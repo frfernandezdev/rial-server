a9c62a6181c87fccdba6c53019af639f7351918a
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 08:56:01 -0400

    feat(shared): added DomainEventSubscriber

---
+++ src/shared/main/com/rial/shared/domain/bus/event/DomainEventSubscriber.java
@@ -0,0 +1,10 @@
+package com.rial.shared.domain.bus.event;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface DomainEventSubscriber {
+    Class<? extends DomainEvent>[] value();
+}
