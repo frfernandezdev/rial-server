7f6d84f90e6b47d139c7852de61216518df97dad
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 08:54:28 -0400

    feat(shared): added QueryHandlerExecutionError

---
+++ src/shared/main/com/rial/shared/domain/bus/query/QueryHandlerExecutionError.java
@@ -0,0 +1,7 @@
+package com.rial.shared.domain.bus.query;
+
+public final class QueryHandlerExecutionError extends RuntimeException {
+    public QueryHandlerExecutionError(Throwable cause) {
+        super(cause);
+    }
+}
