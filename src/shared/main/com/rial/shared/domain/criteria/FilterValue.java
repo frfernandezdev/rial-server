2c3af07b972b9a6afa0231e513f23468ace9c8ff
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 08:42:33 -0400

    feat(shared): added FilterValue

---
+++ src/shared/main/com/rial/shared/domain/criteria/FilterValue.java
@@ -0,0 +1,9 @@
+package com.rial.shared.domain.criteria;
+
+import com.rial.shared.domain.criteria.valueobject.StringValueObject;
+
+public final class FilterValue extends StringValueObject {
+    public FilterValue(String value) {
+        super(value);
+    }
+}
