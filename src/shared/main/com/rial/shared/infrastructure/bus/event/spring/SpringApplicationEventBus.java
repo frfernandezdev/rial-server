cb2c3b8694ccbaafedb905c6d0ec200462c533da
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 09:39:54 -0400

    feat(shared): added SpringApplicationEventBus

---
+++ src/shared/main/com/rial/shared/infrastructure/bus/event/spring/SpringApplicationEventBus.java
@@ -0,0 +1,28 @@
+package com.rial.shared.infrastructure.bus.event.spring;
+
+import com.rial.shared.domain.Service;
+import com.rial.shared.domain.bus.event.DomainEvent;
+import com.rial.shared.domain.bus.event.EventBus;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+@Primary
+@Service
+public class SpringApplicationEventBus implements EventBus {
+    private final ApplicationEventPublisher publisher;
+
+    public SpringApplicationEventBus(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(final List<DomainEvent> events) {
+        events.forEach(this::publish);
+    }
+
+    private void publish(final DomainEvent event) {
+        this.publisher.publishEvent(event);
+    }
+}
