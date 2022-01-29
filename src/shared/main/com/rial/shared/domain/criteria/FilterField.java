45468819446499b0dbf3f203bcc5c1dbad972c33
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 08:41:26 -0400

    feat(shared): added FilterField

---
+++ src/shared/main/com/rial/shared/domain/criteria/FilterField.java
@@ -0,0 +1,9 @@
+package com.rial.shared.domain.criteria;
+
+import com.rial.shared.domain.criteria.valueobject.StringValueObject;
+
+public final class FilterField extends StringValueObject {
+    public FilterField(String value) {
+        super(value);
+    }
+}
