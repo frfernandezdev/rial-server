65909cc81b9cb46f609be93a5e1eb9c0655c91e1
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-29 08:48:45 -0400

    feat(shared): added OrderBy

---
+++ src/shared/main/com/rial/shared/domain/criteria/OrderBy.java
@@ -0,0 +1,9 @@
+package com.rial.shared.domain.criteria;
+
+import com.rial.shared.domain.criteria.valueobject.StringValueObject;
+
+public final class OrderBy extends StringValueObject {
+    public OrderBy(String value) {
+        super(value);
+    }
+}
