e885088e8648e95328c1345c03fe5b397735caef
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-30 15:03:06 -0400

    test(apps/backoffice): added HealthCheckGetControllerTestCase

---
+++ apps/test/com/rial/apps/backoffice/controller/health_check/HealthCheckGetControllerTestCase.java
@@ -0,0 +1,24 @@
+package com.rial.apps.backoffice.controller.health_check;
+
+import com.rial.apps.ApplicationTestCase;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+
+public final class HealthCheckGetControllerTestCase extends ApplicationTestCase {
+    private final Logger logger = LoggerFactory.getLogger(HealthCheckGetControllerTestCase.class);
+
+    @Test
+    void checkTheAppIsWorkingOk() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+
+        logger.info("The is running this test");
+
+        assertResponse(
+                "/health-check",
+                200,
+                "{'application': 'backoffice', 'status': 'ok'}",
+                headers);
+    }
+}
