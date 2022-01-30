2ae94bba87c3db55936aead1fc0a2285c0909515
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-30 15:05:52 -0400

    test(apps/backoffice): added HealthCheckGetController

---
+++ apps/main/com/rial/apps/backoffice/controller/HealthCheckGetController.java
@@ -0,0 +1,35 @@
+package com.rial.apps.backoffice.controller;
+
+
+import com.rial.shared.domain.DomainError;
+import com.rial.shared.domain.bus.command.CommandBus;
+import com.rial.shared.domain.bus.query.QueryBus;
+import com.rial.shared.infrastructure.spring.ApiController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class HealthCheckGetController extends ApiController {
+    public HealthCheckGetController(
+            QueryBus queryBus,
+            CommandBus commandBus
+    ) {
+        super(queryBus, commandBus);
+    }
+
+    @GetMapping("/health-check")
+    public HashMap<String, String> index() {
+        HashMap<String, String> status = new HashMap<>();
+        status.put("application", "backoffice");
+        status.put("status", "ok");
+
+        return status;
+    }
+
+    @Override
+    public HashMap<Class<? extends DomainError>, HttpStatus> errorMapping() { return null; }
+}
+
